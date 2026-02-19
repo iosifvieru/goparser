b73fe2238ab9e8420c9354dc1144c042c9e8d0cc
Iosif Vieru <iosifvieru440@gmail.com>
2026-02-19 13:27:28 +0200

    feature: change from single threaded to goroutines
    - make use of go routines to achieve 5.7x faster log parsing, from 38.3
      seconds for a 3.5GB file to 6.7s only by using go routines and
    "map-reduce" approach
    - make the producer will read N lines from the log and send it to workers, N
      was chosen as 4096 but in the future will be read from CLI
    - spawn runtime.NumCPU() workers to help with the parsing

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"runtime"
+	"sync"
 	"time"
 )
 
@@ -15,86 +17,125 @@ type LogEntry struct {
 
 var logPattern = regexp.MustCompile(`^(\S+) (\S+) (\S+) \[([^\]]+)\] "(\S+) ([^"]*) (\S+)" (\d{3}) (\S+) "([^"]*)" "([^"]*)" "([^"]*)"$`)
 
+func mapLogChunkToLogEntries(lines []string) []LogEntry {
+	logEntries := make([]LogEntry, 0, len(lines))
+
+	for _, line := range lines {
+		logEntry, err := parseLine(line)
+		if err != nil {
+			continue
+		}
+		logEntries = append(logEntries, logEntry)
+	}
+
+	return logEntries
+}
+
 func parseLine(line string) (LogEntry, error) {
 	m := logPattern.FindStringSubmatch(line)
-
 	if m == nil {
-		return LogEntry{}, fmt.Errorf("line does not match log pattern: %q\n", string(line))
+		return LogEntry{}, fmt.Errorf("line does not match log pattern: %q\n", line)
 	}
 
-	// index 1 is ipv4 address
 	ipAddr := m[1]
-
-	// index 8 is HTTP status code in regex
 	respCode := m[8]
-
-	logEntry := LogEntry{ipAddr, respCode}
-	return logEntry, nil
+	return LogEntry{ip: ipAddr, respCode: respCode}, nil
 }
 
-func printRequestReport(data map[string]int) {
-	if len(data) == 0 {
-		fmt.Println("the length of data is 0")
-		return
+func readChunks(scanner *bufio.Scanner, chunkSize int, out chan<- []string) error {
+	defer close(out)
+
+	lines := make([]string, 0, chunkSize)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+		if len(lines) >= chunkSize {
+			chunk := make([]string, len(lines))
+			copy(chunk, lines)
+
+			out <- chunk
+			lines = lines[:0]
+		}
 	}
 
-	var totalRequests = 0
-	for _, v := range data {
-		totalRequests += v
+	// send last partial chunk
+	if len(lines) > 0 {
+		chunk := make([]string, len(lines))
+		copy(chunk, lines)
+		out <- chunk
 	}
 
-	fmt.Printf("Total requests: %d\n", totalRequests)
-
-	for k, v := range data {
-		percentage := float64(v) / float64(totalRequests) * 100
-		fmt.Printf("HTTP %s: %d requests (%0.2f %%)\n", k, v, percentage)
-	}
+	return scanner.Err()
 }
 
-func printIpReport(data map[string]int) {
-	if len(data) == 0 {
-		fmt.Println("the length of data is 0")
-		return
-	}
-
-	fmt.Println("Unique addresses:", len(data))
-
-	for k, v := range data {
-		fmt.Printf("IP %s got %d hits.\n", k, v)
+func parseWorker(chunks <-chan []string, entries chan<- []LogEntry, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for chunk := range chunks {
+		parsed := mapLogChunkToLogEntries(chunk)
+		if len(parsed) > 0 {
+			entries <- parsed
+		}
 	}
 }
 
 func GenerateReport(filepath string) error {
 	file, err := os.Open(filepath)
 	if err != nil {
-		return fmt.Errorf("\"%s\" no such file or directory", filepath)
+		return fmt.Errorf("%q no such file or directory", filepath)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
-	respCodeMap := make(map[string]int)
-	ipAddrMap := make(map[string]int)
+	chunkCh := make(chan []string, 16)
+	entryCh := make(chan []LogEntry, 16)
 
+	const N = 4096
 	startTime := time.Now()
-	for scanner.Scan() {
-		logEntry, err := parseLine(scanner.Text())
 
-		if err != nil {
-			fmt.Print(err)
-			continue
-		}
-
-		ipAddrMap[logEntry.ip]++
-		respCodeMap[logEntry.respCode]++
+	var wg sync.WaitGroup
+	numWorkers := runtime.NumCPU()
+	wg.Add(numWorkers)
+	for range numWorkers {
+		go parseWorker(chunkCh, entryCh, &wg)
 	}
-	elapsed := time.Since(startTime)
 
+	go func() {
+		wg.Wait()
+		close(entryCh)
+	}()
+
+	producerErrCh := make(chan error, 1)
+	go func() {
+		producerErrCh <- readChunks(scanner, N, chunkCh)
+	}()
+
+	totalEntries := 0
+	ipMap := make(map[string]int)
+	statusCodesMap := make(map[string]int)
+	for parsedBatch := range entryCh {
+		totalEntries += len(parsedBatch)
+
+		for _, entry := range parsedBatch {
+			ipMap[entry.ip]++
+			statusCodesMap[entry.respCode]++
+		}
+	}
+
+	if err := <-producerErrCh; err != nil {
+		return err
+	}
+
+	elapsed := time.Since(startTime)
 	fmt.Println("Log source:", filepath)
 	fmt.Println("Log parsing time:", elapsed)
+	fmt.Println("Total requests:", totalEntries)
 
-	printRequestReport(respCodeMap)
-	printIpReport(ipAddrMap)
+	for statusCode, count := range statusCodesMap {
+		percentage := float64(count) / float64(totalEntries) * 100
+		fmt.Fprintf(os.Stdout, "HTTP %s: %d requests (%f %%)\n", statusCode, count, percentage)
+	}
+
+	fmt.Println("Unique addresses:", len(ipMap))
 
 	return nil
 }
